b217719d3be90a12e0a4df76ca2cb474e5c1a3dd
CP-21FAR <111728118+CP-21FAR@users.noreply.github.com>
2022-10-03 15:14:26 +0200

    Update morpion.js

--- js/morpion.js
+++ js/morpion.js
@@ -1,63 +1,99 @@
-var turn = true;
-var move = 1;
-
 $(function (){
-   $("td").click(function (){
+    var i = 0
+    var array = [0,1,2,3,4,5,6,7,8];
 
-       if(!$(this).hasClass("symbole_0") && !$(this).hasClass("symbole_1")){
-           $(this).addClass(("symbole_") + ((turn) ? "1" : "0"))
-           turn =! turn
-       }
-       move++;
-            if (Gagnant() == ("symbole_1")) {
-                $('body').append('<div class="gagnant"><span>Gagnant</span> X</div>');
-                $('body').append('<button onclick="location.reload()">Rejouer</button>');
-                $('.gagnant').css('background-color', '#61892f');
-                $('button').css('color', '#61892f');
-            } else if (Gagnant() == ("symbole_0")) {
-                $('body').append('<div class="gagnant"><span>Gagnant</span> O</div>');
-                $('body').append('<button onclick="location.reload()">Rejouer</button>');
-                $('.gagnant').css('background-color', '#ff0000');
-                $('button').css('color', '#ff0000');
+    //pour chaque case
+    $("body").append("<h2 class='info'>&nbsp;</h2>");
+    $("td").each(function (index, element){
+    //Clique dans une case
+    $(this).click(function () {
+
+        //vérifier si la case n'est pas déjà occupé
+        if ($(element).hasClass("symbole_0") || $(element).hasClass("symbole_1") ) {
+        //colorier les bordures des cases occupées en rouge quand on clique dessus
+                $(element).css({"border": "1px solid red"});
+                setTimeout(() => {
+                    $(element).css({"border": "1px solid black"});
+                }, 600);
+            
+        }else {
+            //mettre le symbole rond si $i est paire
+            if (i % 2 != 0) {
+                $(element).addClass("symbole_0")
+                i++
+                array.splice(index,1,"O")
+            //sinon mettre le symbole croix si $i est impaire
+            } else {
+                $(element).addClass("symbole_1")
+                i++
+                array.splice(index,1,"X")
             }
-   })
-});
+        }
+        //Condition de victoire pour les ronds
+        if (i >= 5){
 
-function Gagnant() {
-    cs1 = $("table tr:nth-child(1) td:nth-child(1)").attr('class');
-    cs2 = $("table tr:nth-child(1) td:nth-child(2)").attr('class');
-    cs3 = $("table tr:nth-child(1) td:nth-child(3)").attr('class');
-    cs4 = $("table tr:nth-child(2) td:nth-child(1)").attr('class');
-    cs5 = $("table tr:nth-child(2) td:nth-child(2)").attr('class');
-    cs6 = $("table tr:nth-child(2) td:nth-child(3)").attr('class');
-    cs7 = $("table tr:nth-child(3) td:nth-child(1)").attr('class');
-    cs8 = $("table tr:nth-child(3) td:nth-child(2)").attr('class');
-    cs9 = $("table tr:nth-child(3) td:nth-child(3)").attr('class');
-   
-    if((cs1 == cs2) && (cs2 == cs3)){
-        console.log(cs3);
-        return cs3;
-    } else if ((cs4 == cs5) && (cs5 == cs6)){
-        return cs6;
-    }else if ((cs7 == cs8) && (cs8 == cs9)){
-        return cs9;
-    }
+            if (array[0] === "X" && array[1] === "X" && array[2] === "X" ||
+                array[3] === "X" && array[4] === "X" && array[5] === "X" ||
+                array[6] === "X" && array[7] === "X" && array[8] === "X" ||
+                array[0] === "X" && array[3] === "X" && array[6] === "X" ||
+                array[1] === "X" && array[4] === "X" && array[7] === "X" ||
+                array[2] === "X" && array[5] === "X" && array[8] === "X" ||
+                array[0] === "X" && array[4] === "X" && array[8] === "X" ||
+                array[6] === "X" && array[4] === "X" && array[2] === "X" ){
+                setTimeout(() => {
+                    alert("Le joueur croix a gagné")
+                    //Remettre le tableau par défaut
+                    array = [0,1,2,3,4,5,6,7,8];
+                    //remettre les classes par défaut
+                    $("td").removeClass("symbole_0")
+                    $("td").removeClass("symbole_1")
+                    i = 0
+                })
+            //vérifier si les croix n'ont pas gagné
+            }else if (i > 8){
+                var egalite = 1
+            }
+            //condition de victoire pour les ronds
+            if (array[0] === "O" && array[1] === "O" && array[2] === "O" ||
+                array[3] === "O" && array[4] === "O" && array[5] === "O" ||
+                array[6] === "O" && array[7] === "O" && array[8] === "O" ||
+                array[0] === "O" && array[3] === "O" && array[6] === "O" ||
+                array[1] === "O" && array[4] === "O" && array[7] === "O" ||
+                array[2] === "O" && array[5] === "O" && array[8] === "O" ||
+                array[0] === "O" && array[4] === "O" && array[8] === "O" ||
+                array[6] === "O" && array[4] === "O" && array[2] === "O" ){
+                setTimeout(() => {
+                    alert("Le joueur cercle a gagné")
+                     //remettre le tableau par défaut
+                    array = [0,1,2,3,4,5,6,7,8];
+                    //remettre les classes par défaut
+                    $("td").removeClass("symbole_0")
+                    $("td").removeClass("symbole_1")
+                    i = 0
+                }, 100)
+            }else if (i > 8){
+                 egalite++
+            }
+            if (egalite == 2){
+                setTimeout(() => {
+                    alert("Égalité")
+                    //Remettre le tableau par défaut
+                    array = [0,1,2,3,4,5,6,7,8];
+                    //Remettre les classes par défaut
+                    $("td").removeClass("symbole_0")
+                    $("td").removeClass("symbole_1")
+                    i = 0
+                }, 100)
+            }
+        }
+    })
 
-    else if((cs1 == cs4) && (cs4 == cs7)){
-        return cs7;
-    }else if((cs2 == cs5) && (cs5 == cs8)) {
-        return cs8;
-    }else if((cs3 == cs6) && (cs6 == cs9)) {
-        return  cs9;
-    }
 
-    else if ((cs1 == cs5) && (cs5 == cs9)){
-        return cs9;
-    }else if ((cs3 == cs5) && (cs5 == cs7)) {
-        return cs7;
-    }
-
-    return -1;
-
-}
-
+    })
+    
+    //Bouton Restart et remettre le tableau par défaut
+    $("body").append("<button class='bnut'>Restart</button>")
+    $('.bnut').click(function() {
+        location.reload();
+    })
+})
