4ab900d1b8d57a7b805f787d8048036ef439ae95
CP-21FAR <111728118+CP-21FAR@users.noreply.github.com>
2022-10-03 11:24:33 +0200

    Update morpion.js

--- js/morpion.js
+++ js/morpion.js
@@ -1,30 +1,29 @@
 var turn = true;
 var move = 1;
 
-// Ajout de Cercle
 $(function (){
    $("td").click(function (){
 
        if(!$(this).hasClass("symbole_0") && !$(this).hasClass("symbole_1")){
            $(this).addClass(("symbole_") + ((turn) ? "1" : "0"))
-           turn =! turn // Inverser la valeur de bool
+           turn =! turn
        }
        move++;
-            if (findWinner() == ("symbole_1")) {
-                $('body').append('<div class="winner"><span>Winner</span> X</div>');
-                $('body').append('<button onclick="location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#61892f');
+            if (Gagnant() == ("symbole_1")) {
+                $('body').append('<div class="gagnant"><span>Gagnant</span> X</div>');
+                $('body').append('<button onclick="location.reload()">Rejouer</button>');
+                $('.gagnant').css('background-color', '#61892f');
                 $('button').css('color', '#61892f');
-            } else if (findWinner() == ("symbole_0")) {
-                $('body').append('<div class="winner"><span>Winner</span> O</div>');
-                $('body').append('<button onclick="location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#ff0000');
+            } else if (Gagnant() == ("symbole_0")) {
+                $('body').append('<div class="gagnant"><span>Gagnant</span> O</div>');
+                $('body').append('<button onclick="location.reload()">Rejouer</button>');
+                $('.gagnant').css('background-color', '#ff0000');
                 $('button').css('color', '#ff0000');
             }
    })
 });
 
-function findWinner() {
+function Gagnant() {
     cs1 = $("table tr:nth-child(1) td:nth-child(1)").attr('class');
     cs2 = $("table tr:nth-child(1) td:nth-child(2)").attr('class');
     cs3 = $("table tr:nth-child(1) td:nth-child(3)").attr('class');
@@ -45,7 +44,6 @@ function findWinner() {
         return cs9;
     }
 
-    //check columns
     else if((cs1 == cs4) && (cs4 == cs7)){
         return cs7;
     }else if((cs2 == cs5) && (cs5 == cs8)) {
@@ -53,14 +51,13 @@ function findWinner() {
     }else if((cs3 == cs6) && (cs6 == cs9)) {
         return  cs9;
     }
-    //check diagonals
+
     else if ((cs1 == cs5) && (cs5 == cs9)){
         return cs9;
     }else if ((cs3 == cs5) && (cs5 == cs7)) {
         return cs7;
     }
 
-    //no winner
     return -1;
 
 }
